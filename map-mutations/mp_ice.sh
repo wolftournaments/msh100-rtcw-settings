e05db04b54a9cd8c940666df1bc1f7ad4e73fd8a
wolftournaments <94031877+wolftournaments@users.noreply.github.com>
2021-11-09 23:59:20 -0500

    Copy https://github.com/msh100/rtcw-config

---
+++ map-mutations/mp_ice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/tools.sh"
+
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "225" "-7680 2264 376"
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "315" "-8512 -656 376"
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "315" "-8640 -656 376"
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "270" "-8304 -664 376"
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "270" "-8368 -664 376"
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "315" "-8576 -656 376"
+remove_spawnpoint "${1}" "team_CTF_bluespawn" "315" "-8704 -656 376"
---
+++ map-mutations/te_escape2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/tools.sh"
+
+remove_spawnpoint "${1}" "team_CTF_redplayer" "360" "-5984 1880 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "270" "-5792 2016 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "360" "-5920 1880 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "270" "-5792 1944 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "360" "-5920 1808 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "90" "-5848 1656 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "270" "-5888 2016 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "90" "-5784 1656 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "90" "-5912 1656 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "60" "-5984 1656 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "35" "-5912 1736 224"
+remove_spawnpoint "${1}" "team_CTF_redplayer" "25" "-5992 1760 224"
